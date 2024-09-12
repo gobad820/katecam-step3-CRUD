626d38c0807c21c432b2fa2afc3002a0aa640aef
Kim Sanghae <pelikan@pusan.ac.kr>
2024-09-12 16:14:51 +0900

    feat($project): 프로젝트 생성

---
+++ src/test/java/katecam/step3/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package katecam.step3.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
