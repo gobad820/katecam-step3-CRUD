78ecf14693ae3532ef8f17f4ecf30ffba5685877
Kim Sanghae <pelikan@pusan.ac.kr>
2024-09-18 01:18:45 +0900

    feat(ReservationDto): 매개변수 검증 어노테이션 추가

--- src/main/java/katecam/step3/crud/dto/ReservationDto.java
+++ src/main/java/katecam/step3/crud/dto/ReservationDto.java
@@ -1,16 +1,29 @@
 package katecam.step3.crud.dto;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import katecam.step3.crud.annotation.ValidArrivalLocation;
+import katecam.step3.crud.annotation.ValidDepartureLocation;
+import katecam.step3.crud.annotation.ValidPrice;
+import katecam.step3.crud.annotation.ValidServiceType;
+import katecam.step3.crud.annotation.ValidTransportation;
 import katecam.step3.crud.enumdomain.ServiceType;
 import katecam.step3.crud.enumdomain.Transportation;
+import org.springframework.validation.annotation.Validated;
 
 /**
  * DTO for {@link katecam.step3.crud.domain.Reservation}
  */
-public record ReservationDto(String departureLocation, String arrivalLocation,
-                             LocalDateTime reservationDateTime, ServiceType serviceType,
-                             Transportation transportation, int price) implements
-    Serializable {
+@Validated
+public record ReservationDto(
+    @ValidDepartureLocation String departureLocation,
+    @ValidArrivalLocation String arrivalLocation,
+    @NotNull(message = "예약 일시를 입력해 주십시오.")
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}", message = "잘못된 날짜 형식입니다. 'yyyy-MM-dd HH:mm:ss' 형식으로 입력해주세요.")
+    String reservationDateTime,
+    @ValidServiceType ServiceType serviceType,
+    @ValidTransportation Transportation transportation,
+    @Min(value = 0, message = "가격은 0 이상 입니다.") @ValidPrice String price) {
 
 }
