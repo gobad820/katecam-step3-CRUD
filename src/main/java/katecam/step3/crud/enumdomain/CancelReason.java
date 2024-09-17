d8ce1b9560f0ac7ff8d199fc20b8ffbafd7eebcb
Kim Sanghae <pelikan@pusan.ac.kr>
2024-09-18 01:17:38 +0900

    chore($enumDomain): 파일 포매팅

--- src/main/java/katecam/step3/crud/enumdomain/CancelReason.java
+++ src/main/java/katecam/step3/crud/enumdomain/CancelReason.java
@@ -4,10 +4,10 @@ public enum CancelReason {
     PATIENTCANCEL("피진료자취소"),
     MANAGERCANCEL("매니저취소"),
     NOKCANCEL("보호자취소"),
-    CHANGEOFMIND("단순변심"),// 단순 변심
+    CHANGE_OF_MIND("단순변심"),// 단순 변심
     SECHEDULEDREBOOK("재예약예정"),// 재예약 예정
     NONE("취소안함"),
-    NONEMATCHED("매니저 매치 안됨");// 매니저 매치 안됨
+    NONEMATCHED("매니저매치안됨");// 매니저 매치 안됨
 
     private final String krName;
 
