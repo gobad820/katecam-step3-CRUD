d3307a5248a8d9860f04ce00a7625eb951485301
Kim Sanghae <pelikan@pusan.ac.kr>
2024-09-19 20:21:26 +0900

    feat(ReportRepository): Reservation으로 Report 찾기 기능 추가

--- src/main/java/katecam/step3/crud/repository/ReportRepository.java
+++ src/main/java/katecam/step3/crud/repository/ReportRepository.java
@@ -1,6 +1,8 @@
 package katecam.step3.crud.repository;
 
+import java.util.Optional;
 import katecam.step3.crud.domain.Report;
+import katecam.step3.crud.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -8,5 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long>,
     JpaSpecificationExecutor<Report> {
-
+    public Optional<Report> findByReservation(Reservation reservation);
 }
