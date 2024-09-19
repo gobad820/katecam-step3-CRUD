86044dd1c127f72db29f955ecd51242612e54011
Kim Sanghae <pelikan@pusan.ac.kr>
2024-09-19 20:27:40 +0900

    feat(MealTimeValidator): 복용 시간 validator 생성

---
+++ src/main/java/katecam/step3/crud/validator/MealTimeValidator.java
@@ -0,0 +1,31 @@
+package katecam.step3.crud.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.List;
+import katecam.step3.crud.annotation.ValidMealTime;
+
+public class MealTimeValidator implements ConstraintValidator<ValidMealTime, String> {
+
+    private static final List<String> MEALTIME = List.of("식전", "식후");
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value.isBlank()) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("복용 시간은 필수 입력값입니다.")
+                .addConstraintViolation();
+            return false;
+        }
+
+        if (!MEALTIME.contains(value.trim())) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(
+                    "유효하지 않은 복용 시간입니다. 유효한 복용시간: " + String.join(", ", MEALTIME))
+                .addConstraintViolation();
+            return false;
+        }
+
+        return true;
+    }
+}
